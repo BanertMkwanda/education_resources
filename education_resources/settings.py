e07c455eb86f67a2f96a8ecf1bbb7de6986641b9
BanertMkwanda <BanertMkwanda@gmail.com>
2025-01-16 19:39:45 -1200

    Update settings.py
    adding render to allowed hosts

--- education_resources/settings.py
+++ education_resources/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-5b=m#l(_0z3euk4*!c9@c-k^sz1t7xxau7y!6tmtsmqv43-zrm
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['education-resources-2.onrender.com']
 
 
 # Application definition
